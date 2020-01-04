2eef7b8b9f558210472b3b694176831bca9e6cf3
Nastya <anastrasd@yandex.ru>
2020-01-04 18:28:16 +0300

    last version of git3

--- git3.py
+++ git3.py
@@ -3,12 +3,44 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import random
 from PyQt5 import uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Widget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.add = QtWidgets.QPushButton(self.centralwidget)
+        self.add.setGeometry(QtCore.QRect(260, 30, 321, 41))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.add.setFont(font)
+        self.add.setObjectName("add")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.add.setText(_translate("MainWindow", "Переместить окружность"))
+
+
+
+class Widget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.add.clicked.connect(self.run)
         self.colors = ['yellow', 'black', 'red', 'orange', 'white', 'blue', 'green', 'purple', 'grey']
 
